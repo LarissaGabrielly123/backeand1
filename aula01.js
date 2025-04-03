8ef1bc249593b77fd809b550e9bfca2db9b35b7e
LarissaGabrielly123 <larissagabriellydelfinasbarain@gmail.com>
2025-04-03 16:26:09 -0400

    aulao1

---
+++ aula01.js
@@ -0,0 +1,6 @@
+let valor= 13.141516;
+let texr="meu primeiro projeto em javascript";
+let ativo=true;
+console.log(valor.toFixed(2)); 
+console.log(Text.toUppercase());
+console.log(ActiveXObject.toString());
