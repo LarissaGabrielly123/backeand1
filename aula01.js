0702a247730398a65db3180333329046f308533b
LarissaGabrielly123 <larissagabriellydelfinasbarain@gmail.com>
2025-04-03 17:45:09 -0400

    tarefa1

--- aula01.js
+++ aula01.js
@@ -1,6 +1,29 @@
 let valor= 13.141516;
-let texr="meu primeiro projeto em javascript";
+let Text="meu primeiro projeto em javascript";
 let ativo=true;
-console.log(valor.toFixed(2)); 
-console.log(Text.toUppercase());
-console.log(ActiveXObject.toString());
+console.log(valor.toFixed(2)); //aredonda valor pra duas casas decimais 
+console.log("tipo" +typeof valor); //retorna o tipo da variavel
+console.log(Text.toUppercase()); //converter o texto para  maiusculo
+console.log("tipo" +typeof text); //retornar o tipo da variavel
+console.log(ActiveXObject.toString()); //converter pra texto
+console.log("tipo" +typeof ativo); //retorna o tipo da variavel
+
+let estados=["são paulo", "minas gerais", "rondonia", "acre"];
+let marcasCarro=[];
+marcasCarro.push("tamanho:"+ estados.length);
+console.log(estados[2]);
+console.log(marcasCarro[0]); //concatenizaçao
+console.log("total letras:" +marcasCarro[0]. length);
+console.log(`estados; ${estados[1]}`); //interpolaçao
+
+let usuario; //valor nao definido
+console.log(usuario);
+//typer erro cannot read propert
+let idade;
+console.log(idade+1); //non - not a nunber / node aula01.js
+
+let nula =null;
+console.log(nula+1);
+
+const aula= "javaScript"
+console.log
---
+++ tarefa1.js
@@ -0,0 +1,11 @@
+//node tarefa1.js
+let nome= "larissa";
+let idade="15";
+console.log ("olá, meu nome é larissa e tenho 15anos");
+
+console.log(nome.toUpperCase()); 
+console.log("tipo" +typeof nome);
+
+console.log(idade.number());
+//console.log("tipo"+ idade);
+
