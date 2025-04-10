d9214c4008b1910c73a8381b0e19393f66195f5c
LarissaGabrielly123 <larissagabriellydelfinasbarain@gmail.com>
2025-04-10 16:13:37 -0400

    certo

--- tarefa1.js
+++ tarefa1.js
@@ -1,11 +1,18 @@
 //node tarefa1.js
+//01
 let nome= "larissa";
-let idade="15";
-console.log ("olá, meu nome é larissa e tenho 15anos");
+let idade=15;
+console.log(`olá meu nome é ${nome}, tenho ${idade} anos`); 
 
+//02
 console.log(nome.toUpperCase()); 
-console.log("tipo" +typeof nome);
+console.log(nome.toLowerCase());
 
+//03
 console.log(typeof idade);
 
-
+//04
+let altura= 1.80;
+let pesso= 68;
+let imc= (pesso / (altura* altura).toFixed(2))
+console.log("seu imc e:"+ imc);
