b7b599bbf002b34088d872c03ec0c3b678d1f5a0
LarissaGabrielly123 <larissagabriellydelfinasbarain@gmail.com>
2025-04-10 17:43:41 -0400

    atividade2

---
+++ atividade02.js
@@ -0,0 +1,21 @@
+//01
+let variavel1=3;
+let variavel2=10;
+console.log(variavel1 + variavel2);
+console.log(variavel1 * variavel2);
+console.log(variavel1 / variavel2);
+
+//2
+let pessoa1=22;
+let pessoa2=11;
+let idade= idade >= 12;
+console.log(idade);
+
+//3
+let idad= 17;
+let idades= idad >=18? "maior de idade" : "menor de idade";
+console.log(idades, idad)
+
+//4
+let numero= 20;
+console.log(numero%2==0? "par": "impar");
