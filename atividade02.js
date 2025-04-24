f8ca079b65c716d761153bab0ac4691fb3c06ca4
LarissaGabrielly123 <larissagabriellydelfinasbarain@gmail.com>
2025-04-24 17:35:57 -0400

    nov

--- atividade02.js
+++ atividade02.js
@@ -1,21 +1,24 @@
-//01
-let variavel1=3;
+//01Crie um programa que declare duas variáveis e exiba o resultado da soma, subtração, multiplicação e divisão desses números.
+let variavel1=5;
 let variavel2=10;
 console.log(variavel1 + variavel2);
+console.log(variavel1 - variavel2)
 console.log(variavel1 * variavel2);
 console.log(variavel1 / variavel2);
 
-//2
+//2Declare duas idades e utilize operadores de comparação para verificar se uma pessoa é mais velha que a outra.
 let pessoa1=22;
 let pessoa2=11;
 let idade= idade >= 12;
 console.log(idade);
 
-//3
+//3Crie uma variável idade com um número e use o operador ternário para verificar se o valor armazenado na variável é maior ou igual a 18. Imprima “Maior de idade” ou “Menor de idade”.
 let idad= 17;
 let idades= idad >=18? "maior de idade" : "menor de idade";
-console.log(idades, idad)
+console.log(idad, idades)
 
-//4
+//4Crie uma variável com um número e use o operador ternário para verificar se é par ou ímpar.
 let numero= 20;
 console.log(numero%2==0? "par": "impar");
+
+//node
--- tarefa1.js
+++ tarefa1.js
@@ -1,17 +1,17 @@
 //node tarefa1.js
-//01
+//01 Escreva um programa que declare duas variáveis, “nome” e “idade”, e asimprima em um console em uma frase que diga “Olá, meu nome é [nome]e eu tenho [idade] anos”.
 let nome= "larissa";
 let idade=15;
 console.log(`olá meu nome é ${nome}, tenho ${idade} anos`); 
 
-//02
+//02. Declare uma string e utilize métodos para converter para maiúsculas eminúsculas.
 console.log(nome.toUpperCase()); 
 console.log(nome.toLowerCase());
 
-//03
+//03Declare uma variável e verifique se o tipo dela é number.
 console.log(typeof idade);
 
-//04
+//04 Calcule o Índice de Massa Corporal (IMC) utilizando variáveis para altura e peso
 let altura= 1.80;
 let pesso= 68;
 let imc= (pesso / (altura* altura).toFixed(2))
